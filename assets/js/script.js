585724deaf090c176138fa53c9fb13b4a2ed2cf1
Zchun <z.c.chun2003@gmail.com>
2022-06-12 16:24:45 -0500

    added more indepth call and skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ const key = "&appid=600ed8a4214e3c2ac55bea638db12b58"
 
 let date = new Date();
 
-$("#searchTerm").keypress(function(event) { 
-	
+$("#cityName").keypress(function(event) { 	
 	if (event.keyCode === 13) { 
 		event.preventDefault();
 		$("#searchBtn").click(); 
@@ -24,5 +23,35 @@ $("#searchBtn").on("click", function() {
 
     const callUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + key;
 
-    console.log(callUrl);
-});
+    $.ajax({
+        url: callUrl,
+        method: "GET"
+      })
+      .then(function (response){
+    
+        console.log(response)
+    
+        console.log(response.name)
+        console.log(response.weather[0].icon)
+    
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        console.log(Math.floor(tempF))
+    
+        console.log(response.main.humidity)
+    
+        console.log(response.wind.speed)
+        })
+      });
+
+      //make list
+
+      //function to get conditions
+
+      //get temp and convert to fahrenheit
+
+      //function to set content
+
+      //append application
+
+      //function to get forcast
+
