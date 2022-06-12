fb518ee235645d29016431a32504030bd74b2766
Zchun <z.c.chun2003@gmail.com>
2022-06-12 16:37:13 -0500

    created function for current conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,18 +40,43 @@ $("#searchBtn").on("click", function() {
         console.log(response.main.humidity)
     
         console.log(response.wind.speed)
+        
+        getConditions(response);
+        makeList();
         })
       });
 
       //make list
+      function makeList() {
+        let listItem = $("<li>").addClass("list-group-item").text(city);
+        $(".list").append(listItem);
+      }
 
       //function to get conditions
+      function getConditions (response) {
 
-      //get temp and convert to fahrenheit
-
-      //function to set content
-
-      //append application
-
+        //get temp and convert to fahrenheit
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        tempF = Math.floor(tempF);
+    
+        $('#currentCity').empty();
+    
+        //function to set content
+        const card = $("<div>").addClass("card");
+        const cardBody = $("<div>").addClass("card-body");
+        const city = $("<h4>").addClass("card-title").text(response.name);
+        const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+        const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+        const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+        const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+        const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+    
+        //append application
+        city.append(cityDate, image)
+        cardBody.append(city, temperature, humidity, wind);
+        card.append(cardBody);
+        $("#currentCity").append(card)
+       
+      }
       //function to get forcast
 
